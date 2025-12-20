fde740f5b57b6daa7f1319e127390ac9fa256e18
bala-amypo <bala@amypo.in>
2025-12-20 09:52:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -10,7 +10,7 @@ import com.example.demo.service.UserService;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthController {
+public class AuthController{
 
     private final UserService userService;
 
