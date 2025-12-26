9ef5ef82aea44493e4372d9bc25cda8eb4aac5ef
bala-amypo <bala@amypo.in>
2025-12-26 14:52:48 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/DuplicateDetectionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DuplicateDetectionServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.DuplicateDetectionLogRepository;
 import com.example.demo.service.DuplicateDetectionService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DuplicateDetectionServiceImpl implements DuplicateDetectionService {
 
@@ -17,5 +19,15 @@ public class DuplicateDetectionServiceImpl implements DuplicateDetectionService 
     @Override
     public DuplicateDetectionLog getLog(Long id) {
         return duplicateDetectionLogRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<DuplicateDetectionLog> getLogsForTicket(Long ticketId) {
+        return duplicateDetectionLogRepository.findAll()
+                .stream()
+                .filter(log ->
+                        log.getTicket1() != null && log.getTicket1().getId().equals(ticketId)
+                        || log.getTicket2() != null && log.getTicket2().getId().equals(ticketId))
+                .toList();
     }
 }
